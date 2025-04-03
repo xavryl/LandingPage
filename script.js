f2eb2f0099829a20abd90109382a20b8fa997348
xavryl <johncilbert312@gmail.com>
2025-04-03 21:47:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 60, // Adjust for fixed navbar height
+            behavior: "smooth"
+        });
+    });
+});
